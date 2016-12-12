f9b456349ec910b52a740764e5ab7a69e6ff5c91
Mitch Moore <mamoore9369@gmail.com>
2016-12-12 10:48:05 -0500

    Initial Set of Changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/mamoore4.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for(i = 0; i < response.courses.completed.length; i++)
+      {
+        console.log(response.courses.completed[i]);
+        $('#badges').append("<div class='course'>" +
+                            "<h3>" + response.courses.completed[i].title + "</h3>" +
+                            "<img src='" + response.courses.completed[i].badge + "'/>" +
+                            "<a href='" + response.courses.completed[i].url + "' target='_blank' class='btn btn-primary'>See Course</a>" +
+                            "</div>");
+      }
+    }
+  });
 
 });
